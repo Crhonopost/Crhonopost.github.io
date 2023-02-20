d05d49cd4f746f594f48efec9a5e65959dbed9de
Crhonopost <nath.souvignet@gmail.com>
2023-02-20 11:46:45 +0100

    init des experimentations

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,11 +1,12 @@
-document.addEventListener('scroll', scrollHandler);
+//document.addEventListener('scroll', scrollHandler);
 
 let sectionIdx = 0;
-let listSections = document.querySelectorAll(".section");
+const listSections = document.querySelectorAll(".section");
 const raccourciSections = document.querySelectorAll("nav > a");
 for(let i=0; i<listSections.length; i++){
     raccourciSections[i].addEventListener("click", function(event){
-        sectionIdx=i;
+        console.log(i);
+        changeCurrentSection(i);
     });
 }
 
@@ -35,22 +36,77 @@ function manageScrollForce(){
     if(scrollForce > 200){
         scrollForce = 0;
         if(sectionIdx<listSections.length-1){
-            sectionIdx++;
-            playFalling(10);
+            changeCurrentSection(sectionIdx+1);
         }
     }
     else if(scrollForce < -200){
         scrollForce = 0;
         if(sectionIdx>=1){
-            sectionIdx--;
+            changeCurrentSection(sectionIdx-1);
         }
     }
+}
+
+function changeCurrentSection(newIndex){
+    raccourciSections[sectionIdx].style.backgroundColor = "";
+    raccourciSections[newIndex].style.backgroundColor = "blue";
+    sectionIdx = newIndex;
 }
 
 function adjustScoll(){
     let smoothY = lerp(0, listSections[sectionIdx].getBoundingClientRect().top, 0.05);
     scrollTo(0,document.documentElement.scrollTop + smoothY);
     previousPosY = document.documentElement.scrollTop;
+}
+
+function adjustPosition(targetY, type, duration){
+    if(type === 'decrease'){
+        adjustDecrease(document.documentElement.scrollTop, targetY, performance.now(), duration*1000);
+    }
+}
+
+function adjustDecrease(previousPosY, targetY, startTime, timeout){
+    let smoothY = lerp(previousPosY, targetY, 0.25);
+    console.log(smoothY);
+    scrollTo(0, smoothY);
+    console.log(startTime)
+    if(performance.now() - startTime < timeout){
+        console.log("alo");
+        setTimeout(function(){adjustDecrease(smoothY, targetY, startTime, timeout)}, 10);
+    }
+}
+
+class CameraHandler{
+    constructor(){
+        this.animationType = "direct";
+        this.targetY = 0;
+        this.previousPosY = 0;
+        this.startTime = performance.now();
+        this.timeout = 0;
+    }
+
+    startAnimate(animationType, from, to, duration){
+        this.animationType = animationType;
+        this.previousPosY = from;
+        this.targetY = to;
+        this.startTime = performance.now();
+        this.timeout = duration * 1000;
+        this.id = setInterval(this.animate, 10);
+    }
+
+    animate(){
+        let currentTime = performance.now();
+        console.log(this.animationType);
+        if(this.animationType !== "direct" && currentTime-this.startTime < this.timeout){
+            let smoothY = lerp(this.previousPosY, this.targetY, 0.25);
+            scrollTo(0, smoothY);
+            this.previousPosY = smoothY;
+            console.log(this.startTime);
+        }
+        else{
+            clearInterval(this.id);
+        }
+    }
 }
 
 
