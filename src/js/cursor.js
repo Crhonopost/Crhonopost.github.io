cab1f9f3f0f7948655fc4fc09334667a7af9265d
Crhonopost <nath.souvignet@gmail.com>
2023-03-29 19:17:58 +0200

    paufinnage

--- src/js/cursor.js
+++ src/js/cursor.js
@@ -1,23 +1,47 @@
-const link = document.querySelectorAll('.hover-this');
-const cursor = document.querySelector('.cursor');
+const cursor = document.querySelector('.contour');
+const cursorinner = document.querySelector('.point');
 let offsetY = 0;
 
-const animateit = function (e) {
-    console.log(e);
-};
 
-const editCursor = e => {
-    const { clientX: x, clientY: y } = e;
-    cursor.style.left = -50+ x + 'px';
-    cursor.style.top =  -50 + y + 'px';
-};
+var a = document.querySelectorAll('a');
 
-link.forEach(b => b.addEventListener('mousemove', animateit));
-link.forEach(b => b.addEventListener('mouseleave', animateit));
-
-
-window.addEventListener('mousemove', editCursor);
-window.addEventListener('scroll', () => {
-    offsetY = window.pageYOffset;
-    cursor.style.transform = `translateY(${offsetY}px)`;
+document.addEventListener('mousemove', function(e){
+  var x = e.clientX;
+  var y = e.clientY;
+  cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${offsetY + e.clientY}px - 50%), 0)`
 });
+
+document.addEventListener('mousemove', function(e){
+  var x = e.clientX;
+  var y = e.clientY;
+  cursorinner.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${offsetY + e.clientY}px - 50%), 0)`;
+});
+
+document.addEventListener('mousedown', function(){
+  cursor.classList.add('click');
+  cursorinner.classList.add('cursorinnerhover')
+});
+
+document.addEventListener('mouseup', function(){
+  cursor.classList.remove('click')
+  cursorinner.classList.remove('cursorinnerhover')
+});
+
+a.forEach(item => {
+  item.addEventListener('mouseover', () => {
+    focus();
+  });
+  item.addEventListener('mouseleave', () => {
+    unFocus();
+});
+})
+
+
+function focus(){
+    cursor.classList.add('hover');
+    cursorinner.classList.add('hover');
+}
+function unFocus(){
+    cursor.classList.remove('hover');
+    cursorinner.classList.remove('hover');
+}
