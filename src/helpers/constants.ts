a1bfa9418cc4118e1134b5816a808ff44e301b53
Nathan Souvignet <nath.souvignet@gmail.com>
2023-12-27 18:01:31 +0100

    working contact form

---
+++ src/helpers/constants.ts
@@ -0,0 +1,4 @@
+export const emailApiKey = process.env.VUE_APP_EMAIL_JS_API;
+export const emailURL = 'https://api.emailjs.com/api/v1.0/email/send'
+export const serviceId = 'service_yi83l7l'
+export const templateId = 'template_aq8k0bx'
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,29 +1,122 @@
 <template>
-    <div>
-        <form>
+    <div id="contact">
+        <form class="frontColor">
             <div>
                 <label for="form_nom">Nom</label>
-                <input type="text" id="form_nom" name="nom" placeholder="Votre nom">
+                <input type="text" id="form_nom" name="nom" placeholder="Votre nom" v-model="state.firstName">
             </div>
             <div>
                 <label for="form_prenom">Prénom</label>
-                <input type="text" id="form_prenom" name="prenom" placeholder="Votre prénom">
+                <input type="text" id="form_prenom" name="prenom" placeholder="Votre prénom" v-model="state.lastName">
             </div>
             <div>
                 <label for="form_email">Email</label>
-                <input type="email" id="form_email" name="email" placeholder="Votre email">
+                <input type="email" id="form_email" name="email" placeholder="Votre email" v-model="state.email">
             </div>
             <div>
                 <label for="form_message">Message</label>
-                <textarea id="form_message" name="message" placeholder="Votre message"></textarea>
+                <textarea id="form_message" name="message" placeholder="Votre message" v-model="state.message"></textarea>
             </div>
             <div>
-                <input type="submit" value="Envoyer">
+                <input id="sub" value="Envoyer" @click="sendForm">
             </div>
         </form>
     </div>
 </template>
 
 <script setup lang="ts">
+import {emailApiKey, templateId, serviceId, emailURL} from "@/helpers/constants"
+import { reactive, ref } from "vue";
+
+
+const state = reactive({
+    firstName: '',
+    lastName: '',
+    email: '',
+    message: ''
+})
+
+
+async function sendForm() {
+    const data = {
+        service_id: serviceId,
+        template_id: templateId,
+        user_id: emailApiKey,
+        template_params: {
+            first_name: state.firstName,
+            last_name: state.lastName,
+            user_email: state.email,
+            message: state.message
+        }
+    }
+
+    const request = await fetch(emailURL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data),
+    })
+
+    if(request.status === 200) {
+        alert('Votre message a bien été envoyé !')
+    } else {
+        alert('Une erreur est survenue, veuillez réessayer plus tard.')
+    }
+}
 
 </script>
+
+<style scoped>
+#contact {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+}
+
+form {
+    border-radius: 5px;
+    padding: 20px;
+    width: 80%;
+}
+
+label {
+    display: block;
+    margin-bottom: 5px;
+    margin-top: 10px;
+    text-align: left;
+}
+
+input {
+    border: 1px solid #ccc;
+    border-radius: 3px;
+    box-sizing: border-box;
+    font-size: 16px;
+    padding: 10px 15px;
+    width: 100%;
+}
+
+textarea {
+    border: 1px solid #ccc;
+    border-radius: 3px;
+    box-sizing: border-box;
+    font-size: 16px;
+    padding: 10px 15px;
+    width: 100%;
+    height: 200px;
+    font-family: "Montserrat", sans-serif;
+}
+
+#sub {
+    background-color: #0f172a;
+    border: none;
+    border-radius: 5px;
+    color: white;
+    cursor: pointer;
+    font-size: 16px;
+    padding: 10px 15px;
+    width: 100px;
+}
+</style>
