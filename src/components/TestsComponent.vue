f68832598d4d0aff6cc78c816b29ee7370161f3b
Crhonopost <nath.souvignet@gmail.com>
2023-12-31 13:09:19 +0100

    got something with advection I will try other idea for negative values

--- src/components/TestsComponent.vue
+++ src/components/TestsComponent.vue
@@ -11,12 +11,24 @@ const canva = ref(null)
 onMounted(() => {
     const regl = require('regl')(canva.value)
 
+    let prevTime = 0;
+
 
     const RADIUS = 512
     const INITIAL_CONDITIONS = (Array(RADIUS * RADIUS * 4)).fill(0).map(
     () => Math.random() > 0.9 ? 255 : 0)
 
     const pressure = (Array(2)).fill(0).map(() =>
+    regl.framebuffer({
+        color: regl.texture({
+            radius: RADIUS,
+            data: INITIAL_CONDITIONS,
+            wrap: 'repeat'
+            }),
+        depthStencil: false
+    }))
+
+    const vectors = (Array(2)).fill(0).map(() =>
     regl.framebuffer({
         color: regl.texture({
         radius: RADIUS,
@@ -26,25 +38,70 @@ onMounted(() => {
         depthStencil: false
     }))
 
-    const updateLife = regl({
+
+    const updateVectorField = regl({
+        frag: `
+        precision highp float;
+        uniform sampler2D prevVector;
+        uniform vec2 resolution;
+        varying vec2 uv;
+
+        // vec2 translateToVector(vec2 vector){
+        //     return vector + vec2(0.5,0.5);
+        // }
+
+        void main(){
+            if(uv.x > 0.45 && uv.x < 0.55 && 
+            uv.y > 0.49 && uv.y < 0.51){
+                vec2 vector = vec2(0.1, 0.);
+                gl_FragColor = vec4(vector, 0., 0.);
+            }
+        }`,
+        framebuffer: ({tick}: {tick: number}) => vectors[(tick + 1) % 2],
+    })
+
+    const updatePressure = regl({
     frag: `
-    precision mediump float;
+    precision highp float;
     uniform sampler2D prevPressure;
+    uniform sampler2D prevVector;
     uniform vec2 resolution;
+    uniform float delta;
     varying vec2 uv;
+
+    vec4 lerp(vec4 from, vec4 to, float delta){
+        return from + (to - from) * delta;
+    }
+
+    vec2 translateFromVector(vec2 vector){
+        return (vector * 2.) - 1.;
+    }
+
+    vec4 advection() {
+        vec2 vector = texture2D(prevVector, uv).xy * delta;
+
+        vector = translateFromVector(vector);
+
+        return texture2D(prevPressure, uv - vector);
+    }
     
-    vec4 diffusion() {
-        vec2 pixelDiff = 1. / resolution;
+    vec4 diffusion(vec2 pixelDiff, vec4 currentPressure) {
         vec4 up = texture2D(prevPressure, vec2(uv.x, uv.y - pixelDiff.y));
         vec4 down = texture2D(prevPressure, vec2(uv.x, uv.y + pixelDiff.y));
         vec4 left = texture2D(prevPressure, vec2(uv.x - pixelDiff.x, uv.y));
         vec4 right = texture2D(prevPressure, vec2(uv.x + pixelDiff.x, uv.y));
 
-        return (up + down + left + right) / 4.;
+        vec4 average = (up + down + left + right) / 4.;
+
+        return lerp(currentPressure, average, delta * 10.);
     }
     
     void main() {
-        gl_FragColor = diffusion();
+        vec2 pixelDiff = 1. / resolution;
+
+        vec4 newPressure = advection();
+
+        gl_FragColor = diffusion(pixelDiff, newPressure);
     }`,
 
     framebuffer: ({tick}: {tick: number}) => pressure[(tick + 1) % 2]
@@ -54,7 +111,9 @@ onMounted(() => {
     frag: `
     precision mediump float;
     uniform sampler2D prevPressure;
+    uniform sampler2D prevVector;
     uniform vec2 resolution;
+    uniform float delta;
     varying vec2 uv;
     void main() {
         float state = texture2D(prevPressure, uv).r;
@@ -76,6 +135,12 @@ onMounted(() => {
 
     uniforms: {
         prevPressure: ({tick}: {tick: number}) => pressure[tick % 2],
+        prevVector: ({tick}: {tick: number}) => vectors[tick % 2],
+        delta: ({time}: {time: number}) => {
+            let tempPrevTime = prevTime;
+            prevTime = time;
+            return time - tempPrevTime;
+        },
         resolution: [800, 800]
     },
 
@@ -85,10 +150,11 @@ onMounted(() => {
     })
 
     regl.frame(() => {
-    setupQuad(() => {
-        regl.draw()
-        updateLife()
-    })
+        setupQuad(() => {
+            regl.draw()
+            updatePressure()
+            updateVectorField()
+        })
     })
 })
 
